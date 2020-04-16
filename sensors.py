30531ad93c58660676c7ac6bc4cf9c75001757e3
Bulat Giniyatullin <bulat.giniiatullin@gmail.com>
2020-04-17 00:52:38 +0300

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -11,17 +11,37 @@ GPIO_Pin = 9
 
 print('KY-015 sensortest - temperature and humidity')
 
+
 def get_sensor_values():
+    temp_humid = get_from_humid_sensor_or_error()
+    if temp_humid:
+        return temp_humid[0], temp_humid[1], get_pressure()
+
+    
+def get_pressure():
+    return barometer.read_pressure()
+
+
+def get_from_humid_sensor_or_error():
     try:
         humid, temper = Adafruit_DHT.read_retry(DHTSensor, GPIO_Pin)
         if humid is not None and temper is not None:
-            value = 'temperature = {0:0.1f}°C; rel. humidity = {1:0.1f}%; pressure = {2}mmHg'.format(
-                temper, humid, barometer.read_pressure())
-#            print(value)
-            return value
+            return humid, temper
         else:
             print('Error while reading - please wait for the next try!')
-            return ''
+            return None
     except KeyboardInterrupt:
         GPIO.cleanup()
-        return ''
+        return None
+
+
+def get_temp():
+    data = get_from_humid_sensor_or_error()
+    if data:
+        return data[1]
+
+
+def get_humid():
+    data = get_from_humid_sensor_or_error()
+    if data:
+        return data[0]
